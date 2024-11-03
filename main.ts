a76e24edde97f93278338a60e17ba9f00b3f7ffa
hmerritt <harry@mrrtt.me>
2024-11-03 13:53:54 +0000

    feat: on-off icons

--- main.ts
+++ main.ts
@@ -1,8 +1,4 @@
-import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
-
-// ------------------------------------------------------------------------------------
-// Core
-// ------------------------------------------------------------------------------------
+import { App, Plugin, PluginSettingTab, setIcon, Setting } from "obsidian";
 
 interface FullWidthSettings {
 	isToggled: boolean;
@@ -15,23 +11,34 @@ const DEFAULT_SETTINGS: FullWidthSettings = {
 };
 
 export default class FullWidth extends Plugin {
+	// ------------------------------------------
+	// Core
+	// ------------------------------------------
+
+	STYLE_ID = "style-full-width";
+	ICON_ACTIVE = "chevrons-right-left";
+	ICON_OFF = "chevrons-left-right";
+
 	settings: FullWidthSettings;
+	$ribbonIcon: HTMLElement;
 
 	async onload() {
 		await this.loadSettings();
 
-		this.addRibbonIcon(
-			"unfold-horizontal",
-			"Full Width",
-			(evt: MouseEvent) => {
-				toggleFullWidth(this.settings);
-			}
+		this.$ribbonIcon = this.addRibbonIcon(this.ICON_OFF, "Full Width", () =>
+			this.toggleFullWidth()
 		);
+
+		if (this.settings.isToggled) {
+			await this.injectFullWidth();
+		}
 
 		this.addSettingTab(new FullWidthSettingTab(this.app, this));
 	}
 
-	onunload() {}
+	onunload() {
+		this.removeFullWidth();
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign(
@@ -44,11 +51,60 @@ export default class FullWidth extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-// ------------------------------------------------------------------------------------
-// Settings
-// ------------------------------------------------------------------------------------
+	// ------------------------------------------
+	// Full Width
+	// ------------------------------------------
+
+	hasInjected() {
+		return !!document.getElementById(this.STYLE_ID);
+	}
+
+	async injectFullWidth() {
+		if (this.hasInjected()) return;
+
+		const styleElement = document.createElement("style");
+		styleElement.id = this.STYLE_ID;
+		styleElement.innerHTML = `
+		.markdown-preview-view.is-readable-line-width .markdown-preview-sizer,
+		.markdown-source-view.is-readable-line-width:not(.is-rtl) .cm-contentContainer,
+		.markdown-source-view.is-line-wrap.is-readable-line-width .cm-content,
+		.markdown-source-view.is-readable-line-width .CodeMirror,
+		.markdown-preview-view.is-readable-line-width .markdown-preview-sizer,
+		.markdown-source-view.mod-cm6.is-readable-line-width .cm-sizer {
+			max-width: ${this.settings.width}% !important;
+		}
+	`;
+		document.querySelector("head")?.appendChild(styleElement);
+
+		this.settings.isToggled = true;
+		this.setRibbonIcon();
+		await this.saveSettings();
+	}
+
+	async removeFullWidth() {
+		document.getElementById(this.STYLE_ID)?.remove();
+		this.settings.isToggled = false;
+		this.setRibbonIcon();
+		await this.saveSettings();
+	}
+
+	async toggleFullWidth() {
+		if (this.hasInjected()) {
+			await this.removeFullWidth();
+		} else {
+			await this.injectFullWidth();
+		}
+	}
+
+	async setRibbonIcon() {
+		if (this.settings.isToggled) {
+			setIcon(this.$ribbonIcon, this.ICON_ACTIVE);
+		} else {
+			setIcon(this.$ribbonIcon, this.ICON_OFF);
+		}
+	}
+}
 
 class FullWidthSettingTab extends PluginSettingTab {
 	plugin: FullWidth;
@@ -86,50 +142,5 @@ class FullWidthSettingTab extends PluginSettingTab {
 						await this.plugin.saveSettings();
 					})
 			);
-	}
-}
-
-// ------------------------------------------------------------------------------------
-// Full Width
-// ------------------------------------------------------------------------------------
-
-const STYLE_ID = "style-full-width";
-
-function hasInjected() {
-	return !!document.getElementById(STYLE_ID);
-}
-
-async function injectFullWidth(settings: FullWidthSettings) {
-	if (hasInjected()) return;
-
-	const styleElement = document.createElement("style");
-	styleElement.id = STYLE_ID;
-	styleElement.innerHTML = `
-		.markdown-preview-view.is-readable-line-width .markdown-preview-sizer,
-		.markdown-source-view.is-readable-line-width:not(.is-rtl) .cm-contentContainer,
-		.markdown-source-view.is-line-wrap.is-readable-line-width .cm-content,
-		.markdown-source-view.is-readable-line-width .CodeMirror,
-		.markdown-preview-view.is-readable-line-width .markdown-preview-sizer,
-		.markdown-source-view.mod-cm6.is-readable-line-width .cm-sizer {
-			max-width: ${settings.width}% !important;
-		}
-	`;
-	document.querySelector("head")?.appendChild(styleElement);
-
-	this.settings.isToggled = true;
-	await this.saveSettings();
-}
-
-async function removeFullWidth(settings: FullWidthSettings) {
-	document.getElementById(STYLE_ID)?.remove();
-	this.settings.isToggled = false;
-	await this.saveSettings();
-}
-
-async function toggleFullWidth(settings: FullWidthSettings) {
-	if (hasInjected()) {
-		await removeFullWidth(settings);
-	} else {
-		await injectFullWidth(settings);
 	}
 }
